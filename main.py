a30b9de6ac9045f0662d3bf85646209150c64173
NeonPlatinum6251 <168927400+NeonPlatinum6251@users.noreply.github.com>
2024-12-10 09:28:28 +0000

    Update main.py
    so stuff works for now

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from sys import exit
 pygame.init()
 tabletop = pygame.display.set_mode((1200,880))
 pygame.display.set_caption("Warhammer40k_Simulator")
-icon = pygame.image.load('icon.png')
-pygame.display.set_icon(icon)
+#icon = pygame.image.load('icon.png')   #I don't have the file as png yet
+#pygame.display.set_icon(icon)
 
 
 while True:
