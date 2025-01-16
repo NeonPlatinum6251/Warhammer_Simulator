1e5a97fc9bede52cf3479f5f3ae499303b2a0a6f
NeonPlatinum6251 <168927400+NeonPlatinum6251@users.noreply.github.com>
2025-01-16 10:53:05 +0000

    added database and test circle

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
+#import mysql.connector
 import pygame
 from sys import exit
+
 #screen setup
 pygame.init()
-tabletop = pygame.display.set_mode((1200,880))
+tabletop = pygame.display.set_mode((1200,880)) #origin is top left!!!
 pygame.display.set_caption("Warhammer40k_Simulator")
 #icon = pygame.image.load('icon.png')   #I don't have the file as png yet
 #pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((80,80))
-test_surface.fill('Red')
+#Models = mysql.connector.connect(host = "localhost", user = "", password = "") #database not yet created 
+def modelsinit():
+    center = (0,0)
+    singlemodel = pygame.draw.circle(tabletop,'red',center,3,0)
+    
+testcircle = modelsinit()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    tabletop.blit(test_surface,(0,0)) #origin is top left!!!
+    pygame.blit(singlemodel)
     pygame.display.update()
     clock.tick(60)
