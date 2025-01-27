29540e5301c12fc40a6e43730459e83cbb49e42a
joseph <josephmarch70@gmail.com>
2025-01-27 14:25:30 +0000

    I'm cooomintting

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import mysql.connector
+#import mysql.connector
 import pygame
 from sys import exit
 
-#screen setup
-pygame.init()
-tabletop = pygame.display.set_mode((1200,880)) #origin is top left!!!
-pygame.display.set_caption("Warhammer40k_Simulator")
-#icon = pygame.image.load('icon.png')   #I don't have the file as png yet
-#pygame.display.set_icon(icon)
+def screensetup():#screen setup
+    pygame.init()
+    tabletop = pygame.display.set_mode((1200,880)) #origin is top left!!!
+    pygame.display.set_caption("Warhammer40k_Simulator")
+    #icon = pygame.image.load('icon.png')   #I don't have the file as png yet
+    #pygame.display.set_icon(icon)
+    pygame.draw.rect(tabletop,(176,176,176),(0,0,100,1200))
 clock = pygame.time.Clock()
-Models = mysql.connector.connect(host = "localhost", user = "", password = "") #database not yet created
-mycursor = Models.cursor()
+# Models = mysql.connector.connect(host = "localhost", user = "", password = "") #database not yet created
+#mycursor = Models.cursor()
+screensetup()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,3 +21,5 @@ while True:
     pygame.display.update()
     clock.tick(60)
 
+class Button(pygame.sprite):
+    def __init__(self,)
