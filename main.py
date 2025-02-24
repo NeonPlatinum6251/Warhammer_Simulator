0c2f465c1bfca8a0e4af695088cd14783d690807
NeonPlatinum6251 <168927400+NeonPlatinum6251@users.noreply.github.com>
2025-02-24 23:01:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #libraries
-from Terrain.py import Terrain
+from Terrain import Terrain
 import pygame
 from sys import exit
 
 #initialise variables and lists
+base_terrain_pieces = []
 base_models = [] #these are the starting models , might make it an array
 pygame.init() #initialises pygame
 tabletop = pygame.display.set_mode((1200,880)) #origin is top left!!! # i could make some tabletops with some pngs 
@@ -13,6 +14,11 @@ pygame.display.set_caption("Warhammer40k_Simulator") #this names the window
 pygame.draw.rect(tabletop,(176,176,176),(0,0,100,1200)) # draw the hotbar make it scrollable by taking the scroll input moving stuff and when it tocuhes a tiny bar at the top to make it disapear
 clock = pygame.time.Clock() 
 mouse = pygame.mouse()
+
+gridsize = 10
+gridwidth = 120
+gridheight = 90
+grid =[[None for _ in range(gridwidth)] for _ in range(gridheight)]
 
 while True:  #this is the main loop where all the display stuff will happen
     for event in pygame.event.get():
@@ -34,8 +40,19 @@ class Button: #for clicking
         self.bheight = bheight
         self.buttontext = buttontext
         self.onclick = onclick
-    
+        self.font = pygame.font.Font(None,24)
+        
+    def draw(self, surface):
+        pygame.draw.rect(surface, (100, 100, 100), (self.x, self.y, self.bwidth, self.bheight))
+        text_surface = self.font.render(self.buttontext, True, (255, 255, 255))
+        surface.blit(text_surface, (self.x + 5, self.y + 5))
 
+    def check_click(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mx, my = pygame.mouse.get_pos()
+            if self.x <= mx <= self.x + self.bwidth and self.y <= my <= self.y + self.bheight:
+                self.onclick()
+    
 def placefirstmodels(basemodels): # places the starting models in the hotbar 
     model_there = True #used to check if there is already a model where its trying to place
     #for i in len(base_models): #does it for each starter model
@@ -45,3 +62,4 @@ def placefirstmodels(basemodels): # places the starting models in the hotbar
             #else: #all good?
                 #drawterrain(placex,placey) #draw it in the place 
                 #model_there = False #makes sure it doesnt do it again
+def 
