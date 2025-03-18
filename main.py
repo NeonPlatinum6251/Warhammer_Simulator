cdf24824f6ebdba54153c471b070affdd1494331
NeonPlatinum6251 <168927400+NeonPlatinum6251@users.noreply.github.com>
2025-03-18 00:52:11 +0000

    Squarepusher theme
    actium - aphex twin

--- main.py
+++ main.py
@@ -6,22 +6,64 @@ class Terrain: #the terrain class where all info for terrain will be
         in_height = None #need to initialise it so it can be used in the loop and same with below
         in_radius = None # ditto 
         valid_shapes = ["square","circle","ellipse"] #these are the shapes that can be used
-        self.name = input("Enter the terrain's name: ")  #the name obvs
-        self.radius = 0#this is not always the radius as i was originally only using circles this is just the size in inches of a shape
-        self.speed = 99999 #this is how much it can move per turn 
-        validheight=False
-        while not validheight: 
-            try: #loop makes sure there is a valid input
-                self.height = int(input("Enter the height of the terrain: ")) #units will able to climb over terrain so height is needed
-                in_height = self.height
-                self.shape = input("Enter the shape of the terrain piece (must be square, circle or elipse): ") #the shape of the terrain duh 
-                in_shape = self.shape
-                self.radius = int(input("Enter the size of the Terrain piece: "))
-                in_radius = self.radius
-                validheight=True 
-            except:
-                print("not set up correctly")      
+        self.name = ""
+        self.radius = 0
+        self.speed = 99999 #this is how much it can move per turn
+        self.shape = ""
+        self.height = ""
         self.onclick = onclick #clicky stuff
+
+        self.name_box = InputBox(300,100,200,32, "Enter Terrain name: ")
+        self.height_box = InputBox(300,150,200,32, "Enter Terrain Height: ")
+        self.shape_box = InputBox(300,200,200,32,"enter Terrain Shape ")
+        self.radius_box = InputBox(300,250,200,32, "enter terrain size: ")
+
+        self.questions= ["name","height", "shape", "radius"]
+        self.currentquestion = 0
+
+    def update(self,event):
+        if self.currentquestion ==0:
+            self.name_box.clickbox(event)
+        elif self.currentquestion == 1:
+            self.height_box.clickbox(event)
+        elif self.currentquestion == 2:
+            self.shape_box.clickbox(event)
+        elif self.currentquestion == 3:
+            self.radius_box.clickbox(event)
+        
+    def draw(self, screen):
+        if self.currentquestion == 0:
+            self.name_box.draw(screen)
+        elif self.currentquestion == 1:
+            self.height_box.draw(screen)
+        elif self.currentquestion == 2:
+            self.shape_box.draw(screen)
+        elif self.currentquestion == 3:
+            self.radius_box.draw(screen)
+
+    def next_question(self):
+        if self.currentquestion == 0 and self.name_box.text:
+            self.name = self.name_box.text
+            self.currentquestion += 1
+        elif self.currentquestion == 1 and self.height_box.text:
+            try:
+                self.height = int(self.height_box.text)
+                self.currentquestion += 1
+            except ValueError:
+                print("Invalid height input!")
+        elif self.currentquestion == 2 and self.shape_box.text:
+            self.shape = self.shape_box.text
+            self.currentquestion += 1
+        elif self.currentquestion == 3 and self.radius_box.text:
+            try:
+                self.radius = int(self.radius_box.text)
+                self.currentquestion += 1
+            except ValueError:
+                print("Invalid radius input!")
+
+    def is_complete(self):
+        return self.currentquestion >= len(self.questions)
+ 
         #self.x = x
         #self.y = y
 
@@ -44,8 +86,9 @@ class Model(Terrain): #terrain is the parent class to models
         self.points = 90
         self.x = 250
         self.y=250
+
     def drawmodel(self,tabletop):
-        pygame.draw.circle(tabletop,(255,0,0),(200,200) ,self.radius)
+        pygame.draw.circle(tabletop,(255,0,0),(self.x,self.y) ,self.radius)
     '''
     def move(self,new_x, new_y, grid):
         grid_x = new_x // 10
@@ -54,62 +97,47 @@ class Model(Terrain): #terrain is the parent class to models
             self.x = new_x
             self.y =new_y
     '''     
+pygame.font.init()
+COLOR_INACTIVE = pygame.Color("blue")
+COLOR_ACTIVE = pygame.Color("green")
+base_font =pygame.font.Font(None,32)
+
 class InputBox:
-    def __init__(self,x,y,w,h,text=''):
-        self.rect = pygame.rect(x,y,w,h)
+    def __init__(self,x,y,w,h,label=""):
+        self.rect = pygame.Rect(x,y,w,h)
         self.color = COLOR_INACTIVE
-        self.text = text 
-        self.txt_surface = FONT.render(text, True, self.color)
+        self.text = ""
+        self.txt_surface = base_font.render(self.text, True, self.color)
         self.active = False
+        self.label = label
     def clickbox(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(event.pos):
                 self.active = not self.active
             else:
                 self.active = False
-            self.color = COlOR_ACTIVE if self.active else COLOR_INACTIVE
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
         if event.type == pygame.KEYDOWN:
             if self.active:
                 if event.key == pygame.K_RETURN:
                     print(self.text)
                     self.text = ''
-                    return text 
                 elif event.key == pygame.K_BACKSPACE:
                     self.text = self.text[:-1]
                 else:
                     self.text += event.unicode
-                self.txt_surface = FONT.render(self.text, True, self.color)
+                self.txt_surface = base_font.render(self.text, True, self.color)
     def update(self):
         width = max(200,self.txt_surface.getwidth()+10)
         self.rect.w = width 
     def draw(self,screen):
-        screen.blit(self.txt_surface, (self.rect.x+5,self.rect.y+5))
+        label_surface = base_font.render(self.label,True,pygame.Color("white"))
+        screen.blit(label_surface, (self.rect.x, self.rect.y -25))
+        screen.blit(self.txt_surface, (self.rect.x + 5,self.rect.y + 5))
         pygame.draw.rect(tabletop,self.color, self.rect, 2)
 
-testtype = InputBox(300,300,100,60,":3")
-'''
-def prompts(tabletop,question):
-    pygame.font.init()
-    current_string =[]
-    display_box(tabletop,question + ": " + string.join(current_string, ""))
-    while True:
-        inkey = get_key()
-        if inkey == K_BACKSPACE:
-            current_string = current_string[0:-1]
-        elif inkey == K_RETURN:
-            break
-        elif inkey == K_MINUS:
-            current_string.append("_")
-        elif inkey <= 127:
-            current_string.append(chr(inkey))
-        display_box(screen, question + ": " + string.join(current_string,""))
-    return string.join(current_string,"")
-'''
 #initialise variables and lists
-pygame.font.init()
-COLOR_INACTIVE = pygame.Color("blue")
-COLOR_ACTIVE = pygame.Color("green")
-base_font =pygame.font.Font(None,32)
+
 base_terrain_pieces = []
 base_models = [] #these are the starting models , might make it an array
 selected_model = None #which model is selected 
@@ -121,24 +149,28 @@ pygame.display.set_caption("Warhammer40k_Simulator") #this names the window
 pygame.draw.rect(tabletop,(176,176,0),(0,0,100,1200)) # draw the hotbar make it scrollable by taking the scroll input moving stuff and when it tocuhes a tiny bar at the top to make it disapear
 clock = pygame.time.Clock() 
 mouse = pygame.mouse
+green = (0,255,0)
 
 gridsize = 10 #the amount of pixels an individual square takes up in the grid  
 gridwidth = 120 #how many squares wide
 gridheight = 90 # how many squares tall
 grid =[[None for _ in range(gridwidth)] for _ in range(gridheight)] #creates the grid as an empty 2D array 
 
-#prompts(tabletop,"what is your name?")
+#test objects
 testmodel = Model()
-
-
+testtype = InputBox(300,300,100,60,'')
+testtype.draw(tabletop)
 while True:  #this is the main loop where all the display stuff will happen
     for event in pygame.event.get(): #when an event happens 
         if event.type == pygame.QUIT: #this loop makes sure python closes properly
             pygame.quit() #stops 
             exit()
-        #if event.type == pygame.MOUSEBUTTONDOWN:
-        #prompts(tabletop,"what is your name?")
-    testmodel.drawmodel(tabletop)
+        testmodel.update(event)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+            testmodel.next_question()
+    testmodel.draw(tabletop)
+    if testmodel.is_complete():
+          testmodel.drawmodel(tabletop)
     pygame.display.update() #updates whats on the screen
     clock.tick(60)
 
@@ -174,5 +206,4 @@ def placefirstmodels(basemodels): # places the starting models in the hotbar
                 #drawterrain(placex,placey) #draw it in the place 
                 #model_there = False #makes sure it doesnt do it again
 #def drag_n_drop():
-'''
 '''
